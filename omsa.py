5473a3f33c9388be4c88872e4b1352c559fff0c5
James Munnelly <james.munnelly@gmail.com>
2015-06-15 11:33:09 +0100

    add 'self' paramter to lambda functions

--- omsa.py
+++ omsa.py
@@ -1,12 +1,12 @@
 from check_dell import parse_om
 
 class storage:
-    controllers = lambda: controller(parse_om("storage controller"))
+    controllers = lambda self: controller(parse_om("storage controller"))
 
     class controller:
-        vdisks = lambda: parse_om("storage vdisk controller=%s" % (self.ID))
-        pdisks = lambda: parse_om("storage pdisk controller=%s" % (self.ID))
-        battery = lambda: parse_om("storage battery controller=%s" % (self.ID))
+        vdisks = lambda self: parse_om("storage vdisk controller=%s" % (self.ID))
+        pdisks = lambda self: parse_om("storage pdisk controller=%s" % (self.ID))
+        battery = lambda self: parse_om("storage battery controller=%s" % (self.ID))
 
         def __init__(self, props):
             for x, y in props.items():
