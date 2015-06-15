25730826df6c10c61c914879c885407ecb0047a9
James Munnelly <james.munnelly@gmail.com>
2015-06-15 11:58:16 +0100

    fixed up pdisks, vdisks and battery method

--- omsa.py
+++ omsa.py
@@ -6,6 +6,18 @@ class storage:
         pdisks = lambda self: parse_om("storage pdisk controller=%s" % (self.ID))
         battery = lambda self: parse_om("storage battery controller=%s" % (self.ID))
 
+        def _get_properties(self, type):
+            return parse_om("storage %s controller=%s" % (type, self.ID))
+
+        def vdisks(self):
+            return self._get_properties('vdisk')
+
+        def pdisks(self):
+            return self._get_properties('pdisk')
+
+        def battery(self):
+            return self._get_properties('battery')
+
         def __init__(self, props):
             for x, y in props.items():
                 setattr(self, x, y)
