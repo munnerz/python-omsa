b454e7cc5f756dea32aa2959a9640e673f12e30a
James Munnelly <james.munnelly@gmail.com>
2015-06-15 11:19:05 +0100

    changed how controllers are defined

--- omsa.py
+++ omsa.py
@@ -1,10 +1,7 @@
 from check_dell import parse_om
 
 class storage:
-
-    class controllers:
-        def list():
-            return parse_om("storage controller", ["ID"])
+    controllers = parse_om("storage controller", ["ID"])
 
     class types:
         vdisk = 'vdisk'
