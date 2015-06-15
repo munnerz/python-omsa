9edc8a7c73f81547f0a006f12cfe34bb96dbd88f
James Munnelly <james.munnelly@gmail.com>
2015-06-15 12:06:45 +0100

    remove old code. add load_props function to load object properties from dict. added vdisks(), pdisks() and battery()

--- omsa.py
+++ omsa.py
@@ -1,26 +1,37 @@
 from check_dell import parse_om
 
+def load_props(obj, props):
+    for x, y in props.items():
+        setattr(obj, x, y)
+
 class storage:
     class Controller:
-        vdisks = lambda self: parse_om("storage vdisk controller=%s" % (self.ID))
-        pdisks = lambda self: parse_om("storage pdisk controller=%s" % (self.ID))
-        battery = lambda self: parse_om("storage battery controller=%s" % (self.ID))
+        class PhysicalDisk:
+            def __init__(self, props):
+                load_props(self, props)
+
+        class VirtualDisk:
+            def __init__(self, props):
+                load_props(self, props)
+
+        class Battery:
+            def __init__(self, props):
+                load_props(self, props)
 
         def _get_properties(self, type):
             return parse_om("storage %s controller=%s" % (type, self.ID))
 
         def vdisks(self):
-            return self._get_properties('vdisk')
+            return map(self.__class__.VirtualDisk, self._get_properties('vdisk'))
 
         def pdisks(self):
-            return self._get_properties('pdisk')
+            return map(self.__class__.PhysicalDiskisk, self._get_properties('pdisk'))
 
         def battery(self):
-            return self._get_properties('battery')
+            return map(self.__class__.Battery, self._get_properties('battery'))
 
         def __init__(self, props):
-            for x, y in props.items():
-                setattr(self, x, y)
+            load_props(self, props)
 
         def __repr__(self):
             return "Controller #%s" % self.ID
