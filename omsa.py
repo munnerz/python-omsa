5823df0441abab4cc613658d11455b1414d862ab
James Munnelly <james.munnelly@gmail.com>
2015-06-15 11:22:08 +0100

    remove omreport filters

--- omsa.py
+++ omsa.py
@@ -1,7 +1,7 @@
 from check_dell import parse_om
 
 class storage:
-    controllers = parse_om("storage controller", ["ID"])
+    controllers = parse_om("storage controller")
 
     class types:
         vdisk = 'vdisk'
